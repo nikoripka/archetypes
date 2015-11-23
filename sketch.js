f0d1c22fd52ddbc442d99dc8d3fc245b88f5a3cc
Nikolas Ripka <nikoripka@outlook.com>
2015-11-23 14:47:14 +0100

    first draft

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var arch;
+
+function preload(){
+  var url = 'event.json';
+  arch = loadJSON(url);
+}
+
+function setup(){
+  var canvas = createCanvas(300,100);
+  canvas.parent('sketch');
+
+  var name = [];
+  var nature = [];
+
+  // this is a special loop for objects: https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Statements/for...in
+  for(var key in events){
+    name.push(events[key].name);
+    nature.push(events[key].nature);
+  }
+
+  console.log(name);
+  console.log(nature);
+}
+
+function draw(){
+}
